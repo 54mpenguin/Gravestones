e80fedef093e97db8a5ea210aae4759600645e4d
BuildTools <unconfigured@null.spigotmc.org>
2018-10-03 18:10:36 +1300

    Removed unnecessary reload command and change the way the test command works

--- src/me/fiftyfour/gravestones/commands/GravesAdmin.java
+++ src/me/fiftyfour/gravestones/commands/GravesAdmin.java
@@ -1,24 +1,19 @@
 package me.fiftyfour.gravestones.commands;
 
-import com.gmail.filoghost.holographicdisplays.api.Hologram;
-import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
-import me.fiftyfour.gravestones.Gravestone;
 import me.fiftyfour.gravestones.Main;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.Plugin;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class GravesAdmin implements CommandExecutor {
     private Plugin plugin = Main.getPlugin(Main.class);
@@ -34,53 +29,37 @@ public class GravesAdmin implements CommandExecutor {
                 return false;
             }
             if (args.length == 0) {
-                player.sendMessage(ChatColor.RED + "Usage: /gravesadmin <reload|test>");
+                player.sendMessage(ChatColor.RED + "Usage: /gravesadmin <test|reincarnate>");
                 return false;
             }
-            if(args[0].equals("reload")) {
-                plugin.reloadConfig();
-                player.sendMessage(ChatColor.LIGHT_PURPLE + "Config has been reloaded!");
+            if (args[0].equals("test")){
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "Testing grave spawning...(you will die)");
+                player.getInventory().addItem(new ItemStack(Material.STONE, 64));
+                player.giveExp(50);
+                player.setGameMode(GameMode.SURVIVAL);
+                player.setHealth(0);
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "Beginning grave spawning....");
                 return true;
-            }else if (args[0].equals("test")){
-                player.sendMessage(ChatColor.LIGHT_PURPLE + "Saving test grave!");
-                int graveNumber;
-                if(Main.graves.get(player.getUniqueId()) != null) {
-                    graveNumber = Main.graves.get(player.getUniqueId()).toArray().length;
-                }else{
-                    graveNumber = 0;
-                }
-                Gravestone grave = new Gravestone();
-                final ArrayList<ItemStack> armorCont = new ArrayList<>(Arrays.asList(player.getInventory().getArmorContents()));
-                final ArrayList<ItemStack> invCont = new ArrayList<>();
-                for(ItemStack stack : player.getInventory().getStorageContents()){
-                    if(stack != null) invCont.add(stack);
-                }
-                grave.setArmor(armorCont);
-                grave.setItems(invCont);
-                grave.setEXPLevel(player.getTotalExperience());
-                grave.setLocation(player.getLocation());
-                grave.setOwner(player.getUniqueId());
-                grave.setNumber(graveNumber + 1);
-                grave.setTimeTillExpire(10);
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-                LocalDateTime now = LocalDateTime.now();
-                Location holoLoc = player.getLocation().clone();
-                holoLoc.add(0.5, 3, 0.5);
-                Hologram hologram = HologramsAPI.createHologram(plugin, holoLoc);
-                ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
-                SkullMeta sm = (SkullMeta) skull.getItemMeta();
-                sm.setOwningPlayer(player);
-                skull.setItemMeta(sm);
-                hologram.appendItemLine(skull);
-                hologram.appendTextLine(ChatColor.GOLD + "" + ChatColor.BOLD + player.getName() + "'s Grave");
-                hologram.appendTextLine(ChatColor.GOLD + "Time of Death: " + dtf.format(now));
-                hologram.appendTextLine(ChatColor.GRAY + "" + ChatColor.ITALIC + "They died");
-                grave.setHologram(hologram);
-                Gravestone.createGrave(grave);
-                player.sendMessage(ChatColor.LIGHT_PURPLE + "Test grave saved at current location with current inventory, armor and exp!");
-                return true;
+            }else if (args[0].equals("reincarnate")) {
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "You're corpse has been REINCARNATED!!");
+                Zombie zombie = (Zombie) player.getLocation().getWorld().spawnEntity(player.getLocation(), EntityType.ZOMBIE);
+                zombie.setBaby(false);
+                zombie.setCustomName(player.getName() + " reincarnated");
+                zombie.setRemoveWhenFarAway(false);
+                zombie.setCanPickupItems(false);
+                zombie.setCustomNameVisible(true);
+                zombie.setTarget(player);
+                zombie.setHealth(20);
+                zombie.setInvulnerable(true);
+                zombie.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 100, 1));
+                zombie.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1000000000, 2));
+                zombie.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 1000000000, 1));
+                zombie.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 1000000000, 1));
+                zombie.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 100, 5));
+                player.getLocation().getWorld().strikeLightningEffect(player.getLocation());
+                zombie.setInvulnerable(false);
             }else{
-                player.sendMessage(ChatColor.RED + "Usage: /gravesadmin <reload|test>");
+                player.sendMessage(ChatColor.RED + "Usage: /gravesadmin <test|reincarnate>");
                 return false;
             }
         }
